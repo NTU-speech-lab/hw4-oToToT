80b1ab90462de9047ed2bef1c31094fd14ac3e94
oToToT <ty1208chiang@gmail.com>
2020-04-27 03:14:27 +0800

    update python files

--- train.py
+++ train.py
@@ -1,10 +1,314 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Recurrent Neural Networks
+# 
+# 本次作業是要讓同學接觸 NLP 當中一個簡單的 task —— 語句分類（文本分類）
+# 
+# 給定一個語句，判斷他有沒有惡意（負面標 1，正面標 0）
+# 
+# 若有任何問題，歡迎來信至助教信箱 ntu-ml-2020spring-ta@googlegroups.com
+
+# In[1]:
+
+
+# from google.colab import drive
+# drive.mount('/content/drive')
+# path_prefix = 'drive/My Drive/Colab Notebooks/hw4 - Recurrent Neural Network'
+path_prefix = './'
+
+
+# ### Download Dataset
+# 有三個檔案，分別是 training_label.txt、training_nolabel.txt、testing_data.txt
+# 
+# - training_label.txt：有 label 的 training data（句子配上 0 or 1，+++$+++ 只是分隔符號，不要理它）
+#     - e.g., 1 +++$+++ are wtf ... awww thanks !
+# 
+# - training_nolabel.txt：沒有 label 的 training data（只有句子），用來做 semi-supervised learning
+#     - ex: hates being this burnt !! ouch
+# 
+# - testing_data.txt：你要判斷 testing data 裡面的句子是 0 or 1
+# 
+#     >id,text  
+#     >0,my dog ate our dinner . no , seriously ... he ate it .  
+#     >1,omg last day sooon n of primary noooooo x im gona be swimming out of school wif the amount of tears am gona cry  
+#     >2,stupid boys .. they ' re so .. stupid !
+
+# In[2]:
+
+
+# !wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1dPHIl8ZnfDz_fxNd2ZeBYedTat2lfxcO' -O 'drive/My Drive/Colab Notebooks/hw8-RNN/data/training_label.txt'
+# !wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1x1rJOX_ETqnOZjdMAbEE2pqIjRNa8xcc' -O 'drive/My Drive/Colab Notebooks/hw8-RNN/data/training_nolabel.txt'
+# !wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=16CtnQwSDCob9xmm6EdHHR7PNFNiOrQ30' -O 'drive/My Drive/Colab Notebooks/hw8-RNN/data/testing_data.txt'
+
+# !gdown --id '1lz0Wtwxsh5YCPdqQ3E3l_nbfJT1N13V8' --output data.zip
+# !unzip data.zip
+# !ls
+
+
+# In[3]:
+
+
+# this is for filtering the warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# ### Utils
+
+# In[4]:
+
+
+# utils.py
+# 這個 block 用來先定義一些等等常用到的函式
+import torch
+import numpy as np
+import pandas as pd
+import torch.optim as optim
+import torch.nn.functional as F
+
+def load_training_data(path='training_label.txt'):
+    # 把 training 時需要的 data 讀進來
+    # 如果是 'training_label.txt'，需要讀取 label，如果是 'training_nolabel.txt'，不需要讀取 label
+    if 'training_label' in path:
+        with open(path, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+            lines = [line.strip('\n').split(' ') for line in lines]
+        x = [line[2:] for line in lines]
+        y = [line[0] for line in lines]
+        return x, y
+    else:
+        with open(path, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+            x = [line.strip('\n').split(' ') for line in lines]
+        return x
+
+def load_testing_data(path='testing_data'):
+    # 把 testing 時需要的 data 讀進來
+    with open(path, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        X = ["".join(line.strip('\n').split(",")[1:]).strip() for line in lines[1:]]
+        X = [sen.split(' ') for sen in X]
+    return X
+
+def evaluation(outputs, labels):
+    # outputs => probability (float)
+    # labels => labels
+    outputs[outputs>=0.5] = 1 # 大於等於 0.5 為有惡意
+    outputs[outputs<0.5] = 0 # 小於 0.5 為無惡意
+    correct = torch.sum(torch.eq(outputs, labels)).item()
+    return correct
+
+
+# ### Train Word to Vector
+
+# In[5]:
+
+
+# w2v.py
+# 這個 block 是用來訓練 word to vector 的 word embedding
+# 注意！這個 block 在訓練 word to vector 時是用 cpu，可能要花到 10 分鐘以上
+import os
+import numpy as np
+import pandas as pd
+import argparse
+from gensim.models import word2vec
+
+def train_word2vec(x):
+    # 訓練 word to vector 的 word embedding
+    model = word2vec.Word2Vec(x, size=500, window=5, min_count=5, workers=12, iter=25, sg=1)
+    return model
+
+if __name__ == "__main__" and False:
+    print("loading training data ...")
+    train_x, y = load_training_data('training_label.txt')
+    train_x_no_label = load_training_data('training_nolabel.txt')
+
+    print("loading testing data ...")
+    test_x = load_testing_data('testing_data.txt')
+
+    model = train_word2vec(train_x + train_x_no_label + test_x)
+#     model = train_word2vec(train_x + test_x)
+    
+    print("saving model ...")
+    # model.save(os.path.join(path_prefix, 'model/w2v_all.model'))
+    model.save(os.path.join(path_prefix, 'w2v_all.model'))
+
+
+# ### Data Preprocess
+
+# In[6]:
+
+
+# preprocess.py
+# 這個 block 用來做 data 的預處理
+from torch import nn
+from gensim.models import Word2Vec
+
+class Preprocess():
+    def __init__(self, sentences, sen_len, w2v_path="./w2v.model"):
+        self.w2v_path = w2v_path
+        self.sentences = sentences
+        self.sen_len = sen_len
+        self.idx2word = []
+        self.word2idx = {}
+        self.embedding_matrix = []
+    def get_w2v_model(self):
+        # 把之前訓練好的 word to vec 模型讀進來
+        self.embedding = Word2Vec.load(self.w2v_path)
+        self.embedding_dim = self.embedding.vector_size
+    def add_embedding(self, word):
+        # 把 word 加進 embedding，並賦予他一個隨機生成的 representation vector
+        # word 只會是 "<PAD>" 或 "<UNK>"
+        vector = torch.empty(1, self.embedding_dim)
+        torch.nn.init.uniform_(vector)
+        self.word2idx[word] = len(self.word2idx)
+        self.idx2word.append(word)
+        self.embedding_matrix = torch.cat([self.embedding_matrix, vector], 0)
+    def make_embedding(self, load=True):
+        print("Get embedding ...")
+        # 取得訓練好的 Word2vec word embedding
+        if load:
+            print("loading word to vec model ...")
+            self.get_w2v_model()
+        else:
+            raise NotImplementedError
+        # 製作一個 word2idx 的 dictionary
+        # 製作一個 idx2word 的 list
+        # 製作一個 word2vector 的 list
+        for i, word in enumerate(self.embedding.wv.vocab):
+            print('get words #{}'.format(i+1), end='\r')
+            #e.g. self.word2index['he'] = 1 
+            #e.g. self.index2word[1] = 'he'
+            #e.g. self.vectors[1] = 'he' vector
+            self.word2idx[word] = len(self.word2idx)
+            self.idx2word.append(word)
+            self.embedding_matrix.append(self.embedding[word])
+        print('')
+        self.embedding_matrix = torch.tensor(self.embedding_matrix)
+        # 將 "<PAD>" 跟 "<UNK>" 加進 embedding 裡面
+        self.add_embedding("<PAD>")
+        self.add_embedding("<UNK>")
+        print("total words: {}".format(len(self.embedding_matrix)))
+        return self.embedding_matrix
+    def pad_sequence(self, sentence):
+        # 將每個句子變成一樣的長度
+        if len(sentence) > self.sen_len:
+            sentence = sentence[:self.sen_len]
+        else:
+            pad_len = self.sen_len - len(sentence)
+            for _ in range(pad_len):
+                sentence.append(self.word2idx["<PAD>"])
+        assert len(sentence) == self.sen_len
+        return sentence
+    def sentence_word2idx(self):
+        # 把句子裡面的字轉成相對應的 index
+        sentence_list = []
+        for i, sen in enumerate(self.sentences):
+            print('sentence count #{}'.format(i+1), end='\r')
+            sentence_idx = []
+            for word in sen:
+                if (word in self.word2idx.keys()):
+                    sentence_idx.append(self.word2idx[word])
+                else:
+                    sentence_idx.append(self.word2idx["<UNK>"])
+            # 將每個句子變成一樣的長度
+            sentence_idx = self.pad_sequence(sentence_idx)
+            sentence_list.append(sentence_idx)
+        return torch.LongTensor(sentence_list)
+    def labels_to_tensor(self, y):
+        # 把 labels 轉成 tensor
+        y = [int(label) for label in y]
+        return torch.LongTensor(y)
+
+
+# ### Dataset
+
+# In[7]:
+
+
+# data.py
+# 實作了 dataset 所需要的 '__init__', '__getitem__', '__len__'
+# 好讓 dataloader 能使用
+import torch
+from torch.utils import data
+
+class TwitterDataset(data.Dataset):
+    """
+    Expected data shape like:(data_num, data_len)
+    Data can be a list of numpy array or a list of lists
+    input data shape : (data_num, seq_len, feature_dim)
+    
+    __len__ will return the number of data
+    """
+    def __init__(self, X, y):
+        self.data = X
+        self.label = y
+    def __getitem__(self, idx):
+        if self.label is None: return self.data[idx]
+        return self.data[idx], self.label[idx]
+    def __len__(self):
+        return len(self.data)
+
+
+# ### Model
+
+# In[8]:
+
+
+# model.py
+# 這個 block 是要拿來訓練的模型
+import torch
+from torch import nn
+class LSTM_Net(nn.Module):
+    def __init__(self, embedding, embedding_dim, hidden_dim, num_layers, dropout=0.5, fix_embedding=True):
+        super(LSTM_Net, self).__init__()
+        # 製作 embedding layer
+        self.embedding = torch.nn.Embedding(embedding.size(0),embedding.size(1))
+        self.embedding.weight = torch.nn.Parameter(embedding)
+        # 是否將 embedding fix 住，如果 fix_embedding 為 False，在訓練過程中，embedding 也會跟著被訓練
+        self.embedding.weight.requires_grad = False if fix_embedding else True
+        self.embedding_dim = embedding.size(1)
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers, dropout=dropout, batch_first=True, bidirectional=True)
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim * 2, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+    def forward(self, inputs):
+        inputs = self.embedding(inputs)
+        x, _ = self.lstm(inputs, None)
+        # x 的 dimension (batch, seq_len, hidden_size)
+        # 取用 LSTM 最後一層的 hidden state
+        x = x[:, -1, :] 
+        x = self.classifier(x)
+        return x
+
+
+# ### Train
+
+# In[9]:
+
+
+# train.py
 # 這個 block 是用來訓練模型的
 import torch
 from torch import nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-def training(batch_size, n_epoch, lr, model_dir, train, valid, model, device):
+def training(batch_size, n_epoch, lr, model_dir, train, valid, model, device, best_acc = 0):
     total = sum(p.numel() for p in model.parameters())
     trainable = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('\nstart training, parameter total:{}, trainable:{}\n'.format(total, trainable))
@@ -13,7 +317,7 @@ def training(batch_size, n_epoch, lr, model_dir, train, valid, model, device):
     t_batch = len(train) 
     v_batch = len(valid) 
     optimizer = optim.Adam(model.parameters(), lr=lr) # 將模型的參數給 optimizer，並給予適當的 learning rate
-    total_loss, total_acc, best_acc = 0, 0, 0
+    total_loss, total_acc = 0, 0
     for epoch in range(n_epoch):
         total_loss, total_acc = 0, 0
         # 這段做 training
@@ -27,10 +331,11 @@ def training(batch_size, n_epoch, lr, model_dir, train, valid, model, device):
             loss.backward() # 算 loss 的 gradient
             optimizer.step() # 更新訓練模型的參數
             correct = evaluation(outputs, labels) # 計算此時模型的 training accuracy
-            total_acc += (correct / batch_size)
+            total_acc += (correct / len(inputs))
             total_loss += loss.item()
             print('[ Epoch{}: {}/{} ] loss:{:.3f} acc:{:.3f} '.format(
-            	epoch+1, i+1, t_batch, loss.item(), correct*100/batch_size), end='\r')
+                epoch+1, i+1, t_batch, loss.item(), correct*100/len(inputs)
+            ), end='\r')
         print('\nTrain | Loss:{:.5f} Acc: {:.3f}'.format(total_loss/t_batch, total_acc/t_batch*100))
 
         # 這段做 validation
@@ -56,3 +361,212 @@ def training(batch_size, n_epoch, lr, model_dir, train, valid, model, device):
                 print('saving model with acc {:.3f}'.format(total_acc/v_batch*100))
         print('-----------------------------------------------')
         model.train() # 將 model 的模式設為 train，這樣 optimizer 就可以更新 model 的參數（因為剛剛轉成 eval 模式）
+    return best_acc
+
+
+# ### Test
+
+# In[10]:
+
+
+# test.py
+# 這個 block 用來對 testing_data.txt 做預測
+import torch
+from torch import nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+def testing(batch_size, test_loader, model, device):
+    model.eval()
+    ret_output = []
+    with torch.no_grad():
+        for inputs in test_loader:
+            inputs = inputs.to(device, dtype=torch.long)
+            outputs = model(inputs)
+            outputs = outputs.squeeze()
+            outputs[outputs>=0.5] = 1 # 大於等於 0.5 為負面
+            outputs[outputs<0.5] = 0 # 小於 0.5 為正面
+            ret_output += outputs.int().tolist()
+    
+    return ret_output
+def get_score(batch_size, test_loader, model, device):
+    model.eval()
+    ret_output = []
+    with torch.no_grad():
+        for inputs in test_loader:
+            inputs = inputs.to(device, dtype=torch.long)
+            outputs = model(inputs)
+            outputs = outputs.squeeze()
+            ret_output += outputs.float().tolist()
+    return ret_output
+
+
+# ### Main
+
+# In[11]:
+
+
+# main.py
+import os
+import torch
+import argparse
+import numpy as np
+from torch import nn
+from gensim.models import word2vec
+from sklearn.model_selection import train_test_split
+
+# 通過 torch.cuda.is_available() 的回傳值進行判斷是否有使用 GPU 的環境，如果有的話 device 就設為 "cuda"，沒有的話就設為 "cpu"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 處理好各個 data 的路徑
+train_with_label = os.path.join(path_prefix, 'training_label.txt')
+train_no_label = os.path.join(path_prefix, 'training_nolabel.txt')
+testing_data = os.path.join(path_prefix, 'testing_data.txt')
+
+w2v_path = os.path.join(path_prefix, 'w2v_all.model') # 處理 word to vec model 的路徑
+
+# 定義句子長度、要不要固定 embedding、batch 大小、要訓練幾個 epoch、learning rate 的值、model 的資料夾路徑
+sen_len = 35
+fix_embedding = True # fix embedding during training
+batch_size = 128
+epoch = 3
+iteration = 5
+sr = 0.05
+lr = 0.001
+vr = 0.25
+# model_dir = os.path.join(path_prefix, 'model/') # model directory for checkpoint model
+model_dir = path_prefix # model directory for checkpoint model
+
+print("loading data ...") # 把 'training_label.txt' 跟 'training_nolabel.txt' 讀進來
+train_x, y = load_training_data(train_with_label)
+train_x_no_label = load_training_data(train_no_label)
+
+# 對 input 跟 labels 做預處理
+preprocess = Preprocess(train_x, sen_len, w2v_path=w2v_path)
+embedding = preprocess.make_embedding(load=True)
+train_x = preprocess.sentence_word2idx()
+y = preprocess.labels_to_tensor(y)
+
+preprocess = Preprocess(train_x_no_label, sen_len, w2v_path=w2v_path)
+embedding = preprocess.make_embedding(load=True)
+train_x_no_label = preprocess.sentence_word2idx()
+
+# 製作一個 model 的對象
+model = LSTM_Net(embedding, embedding_dim=500, hidden_dim=500, num_layers=3, dropout=0.5, fix_embedding=fix_embedding)
+model = model.to(device) # device為 "cuda"，model 使用 GPU 來訓練（餵進去的 inputs 也需要是 cuda tensor）
+
+# 把 data 分為 training data 跟 validation data（將一部份 training data 拿去當作 validation data）
+pivot = int(len(train_x) * vr)
+X_train, X_val, y_train, y_val = train_x[pivot:], train_x[:pivot], y[pivot:], y[:pivot]
+
+best_acc = 0
+
+# 開始訓練
+for _ in range(iteration):
+    # 把 data 做成 dataset 供 dataloader 取用
+    train_dataset = TwitterDataset(X=X_train, y=y_train)
+    val_dataset = TwitterDataset(X=X_val, y=y_val)
+
+    # 把 data 轉成 batch of tensors
+    train_loader = torch.utils.data.DataLoader(
+        dataset = train_dataset,
+        batch_size = batch_size,
+        shuffle = True,
+        #num_workers = 8
+    )
+
+    val_loader = torch.utils.data.DataLoader(
+        dataset = val_dataset,
+        batch_size = batch_size,
+        shuffle = False,
+        #num_workers = 8
+    )
+    best_acc = training(batch_size, epoch, lr, model_dir, train_loader, val_loader, model, device, best_acc)
+
+    train_no_label_dataset = TwitterDataset(X=train_x_no_label, y=None)
+    train_no_label_loader = torch.utils.data.DataLoader(
+        dataset = train_no_label_dataset,
+        batch_size = batch_size,
+        shuffle = False,
+        #num_workers = 8
+    )
+    scores = get_score(batch_size, train_no_label_loader, model, device)
+    indices = np.argsort(scores)
+    indices_needed = np.concatenate((indices[:int(len(scores) * sr)], indices[-int(len(scores) * sr):]))
+    indices_unused = indices[int(len(scores) * sr):-int(len(scores) * sr)]
+    X_train = torch.cat((
+        X_train, 
+        torch.LongTensor([train_x_no_label[idx].numpy() for idx in indices_needed])
+    ))
+    y_train = torch.cat((y_train, torch.LongTensor([scores[idx] >= 0.5 for idx in indices_needed])))
+    train_x_no_label = torch.LongTensor([train_x_no_label[idx].numpy() for idx in indices_unused])
+
+
+# ### Predict and Write to csv file
+
+# In[12]:
+
+
+# 開始測試模型並做預測
+print("loading testing data ...")
+test_x = load_testing_data(testing_data)
+preprocess = Preprocess(test_x, sen_len, w2v_path=w2v_path)
+embedding = preprocess.make_embedding(load=True)
+test_x = preprocess.sentence_word2idx()
+test_dataset = TwitterDataset(X=test_x, y=None)
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                            batch_size = batch_size,
+                                            shuffle = False,
+                                            #num_workers = 8
+                                         )
+print('\nload model ...')
+model = torch.load(os.path.join(model_dir, 'ckpt.model'))
+outputs = testing(batch_size, test_loader, model, device)
+
+# 寫到 csv 檔案供上傳 Kaggle
+tmp = pd.DataFrame({"id":[str(i) for i in range(len(test_x))],"label":outputs})
+print("save csv ...")
+tmp.to_csv(os.path.join(path_prefix, 'predict.csv'), index=False)
+print("Finish Predicting")
+
+# 以下是使用 command line 上傳到 Kaggle 的方式
+# 需要先 pip install kaggle、Create API Token，詳細請看 https://github.com/Kaggle/kaggle-api 以及 https://www.kaggle.com/code1110/how-to-submit-from-google-colab
+# kaggle competitions submit [competition-name] -f [csv file path]] -m [message]
+# e.g., kaggle competitions submit ml-2020spring-hw4 -f output/predict.csv -m "......"
+
+
+# #### Check where the files are
+
+# In[13]:
+
+
+# !pwd
+# !ls
+
+
+# #### Download the files to your computer
+
+# In[14]:
+
+
+# from google.colab import files
+# files.download('predict.csv')
+
+
+# In[15]:
+
+
+torch.cuda.empty_cache()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
