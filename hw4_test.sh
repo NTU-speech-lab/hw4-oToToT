4c2d11a4b00d31033eabda23e733921a06661c36
oToToT <ty1208chiang@gmail.com>
2020-05-06 01:54:25 +0800

    I'm stupid

--- hw4_test.sh
+++ hw4_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#gdown --id '1X3JKne2BumgizwDmDiLG4K1-pExWDHyw' --output w2v_all.model.wv.vectors.npy
+gdown --id '1X3JKne2BumgizwDmDiLG4K1-pExWDHyw' --output w2v_all.model.wv.vectors.npy
 gdown --id '1qQdLtltIxuUqbsec8eWdeMBHaxGLXwsr' --output w2v_all.model.trainables.syn1neg.npy
 gdown --id '1DEg-EtrUuvTwOWtyzBfkIwlx9g-GFQmT' --output w2v_all.model
 gdown --id '1ALj6fwOEFLYz2tcQmPqMDb-D4-wASjZt' --output torch.model
